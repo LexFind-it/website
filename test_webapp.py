4657dbf09247c1bfa602936cc1dbc6ed5191acc9
Paolo Piacenti <paolopiacenti1@gmail.com>
2024-11-08 16:51:06 +0000

    connected to API v2

--- test_webapp.py
+++ test_webapp.py
@@ -122,7 +122,7 @@ for message in st.session_state.messages:
 
 # Create a chat input field to allow the user to enter a message. This will display
 # automatically at the bottom of the page.
-if prompt := st.chat_input("Scrivi un messaggio a LexFind.it"):
+if prompt := st.chat_input("Scrivi un messaggio a TaxFinder"):
 
     # Store and display the current prompt.
     st.session_state.messages.append({"role": "user", "content": prompt})
